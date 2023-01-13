029b5436e80c1f55ed3ea6257cba96a59d8680b0
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 01:38:06 -0500

    Refactor sign-up endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,8 @@
 import express from 'express';
 
-import DataResponse from '../interfaces/DataResponse';
 import MessageResponse from '../interfaces/MessageResponse';
 
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-
-import { Prisma, PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import signUp from './sign-up';
 
 const router = express.Router();
 
@@ -17,34 +12,6 @@ router.get<{}, MessageResponse>('/', (req, res) => {
   });
 });
 
-router.post<{}, DataResponse>('/sign-up', async (req, res) => {
-  const response: DataResponse = { data: null, errors: [] };
-  let { email, name, password } = req.body as Prisma.UserCreateInput;
-
-  if (
-    typeof email !== 'string' ||
-    typeof name !== 'string' ||
-    typeof password !== 'string'
-  ) {
-    response.errors.push({ message: 'Invalid user information has been provided' });
-    return res.status(400).json(response);
-  }
-
-  // hash password
-  const salt = await bcrypt.genSalt();
-  password = await bcrypt.hash(password, salt);
-
-  const user = await prisma.user.create({
-    data: { email, name, password },
-  });
-
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
-    expiresIn: process.env.USER_TOKEN_EXPIRATION_TIME as string,
-  });
-
-  response.data = { ...user, token };
-
-  return res.status(201).json(response);
-});
+router.use('/sign-up', signUp);
 
 export default router;
