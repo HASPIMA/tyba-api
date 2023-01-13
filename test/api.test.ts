4473f984224d1565971779e87d3782f5b5c5e91e
Harrison <harrison.pinto@d4d.com.co>
2023-01-12 23:18:35 -0500

    Add test case for password hashing

--- test/api.test.ts
+++ test/api.test.ts
@@ -4,6 +4,8 @@ import app from '../src/app';
 
 import { faker } from '@faker-js/faker';
 
+import DataResponse from '../src/interfaces/DataResponse';
+
 describe('GET /api/v1', () => {
   it('responds with a json message', (done) => {
     request(app)
@@ -29,7 +31,8 @@ describe('POST /api/v1/sign-up', () => {
   });
 
   it('should create an user if info is provided', (done) => {
-    const userInfo = { email: faker.internet.email(),
+    const userInfo = {
+      email: faker.internet.email(),
       name: faker.name.fullName(),
       password: faker.internet.password(),
     };
@@ -39,5 +42,32 @@ describe('POST /api/v1/sign-up', () => {
       .send(userInfo)
       .expect(201, done);
   });
-});
 
+  it('should hash user\'s password', (done) => {
+    const userInfo = {
+      email: faker.internet.email(),
+      name: faker.name.fullName(),
+      password: faker.internet.password(),
+    };
+
+    request(app)
+      .post('/api/v1/sign-up')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .send(userInfo)
+      .expect(201)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        const { data } = res.body as DataResponse;
+
+        if (data.password === userInfo.password) {
+          return done('Password should not be stored in plain text');
+        }
+
+        return done();
+      });
+  });
+});
