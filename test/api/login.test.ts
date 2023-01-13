0b50758696bc5ebcbf5524f84e1c51a21b7b1a90
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 04:20:07 -0500

    Create test case to verify recent user can login

--- test/api/login.test.ts
+++ test/api/login.test.ts
@@ -1,11 +1,14 @@
 import request from 'supertest';
 
 import app from '../../src/app';
+import { generateMockUser, jwtRegex } from '../helpers';
 
 describe('POST /api/v1/login', () => {
+  const endpointLogin = '/api/v1/login';
+
   it('should fail if no credentials are provided', (done) => {
     request(app)
-      .post('/api/v1/login')
+      .post(endpointLogin)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(
@@ -16,5 +19,33 @@ describe('POST /api/v1/login', () => {
         },
         done,
       );
+  });
+
+  it('should be able to login with recently created user', async () => {
+    const userInfo = generateMockUser();
+
+    // Create user in db
+    const signUp = await request(app)
+      .post('/api/v1/sign-up')
+      .send(userInfo);
+
+    expect(signUp.statusCode).toBe(201);
+    expect(signUp.body).toHaveProperty('data.email');
+
+    // Login with same credentials
+    const login = await request(app)
+      .post(endpointLogin)
+      .set('Accept', 'application/json')
+      .send({ email: userInfo.email, password: userInfo.password });
+
+    expect(login.statusCode).toBe(200);
+
+    // It has to be a valid JWT
+    expect(login.body).toHaveProperty('data.token');
+    expect(login.body.data.token).toMatch(jwtRegex);
+
+    // Emails should match
+    expect(login.body).toHaveProperty('data.email');
+    expect(login.body).toBe(signUp.body.data.email);
   });
 });
