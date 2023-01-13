1573e1db815896faa6b3819b28381d63a475241b
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 16:46:30 -0500

    Refactor middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
-import * as middlewares from './middlewares';
+import * as middlewares from './middlewares/handlers.middleware';
 import api from './routes';
 import MessageResponse from './interfaces/MessageResponse';
 
