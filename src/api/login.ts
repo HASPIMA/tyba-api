590b0c27df353c31a2edcdbb99adcfc330db2c3e
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 05:52:43 -0500

    Pass test case when no credential is provided
    If any error happens when retrieving the user, the error is passed down
    to the error handler

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,13 +2,39 @@ import express from 'express';
 
 import DataResponse from '../interfaces/DataResponse';
 
-import { type User } from '@prisma/client';
+import { PrismaClient, type User } from '@prisma/client';
+const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.post<{}, DataResponse>('/', async (req, res) => {
+router.post<{}, DataResponse>('/', async (req, res, next) => {
   const response: DataResponse = { data: null, errors: [] };
-  let { email,  password } = req.body as User;
+  let { email, password } = req.body as User;
+
+  if (typeof email !== 'string' || typeof password !== 'string') {
+    response.errors.push({
+      message: 'Invalid user information has been provided',
+    });
+    return res.status(400).json(response);
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  }).catch( e => {
+    console.error('Unexpected error retrieving user', e);
+
+    response.errors.push({
+      message: 'Unexpected error retrieving user',
+      stack: e,
+    });
+    res.statusCode = 500;
+
+    return null;
+  });
+
+  if (res.statusCode === 500) {
+    return next(response.errors[0]);
+  }
 
   if (
     typeof email !== 'string' ||
