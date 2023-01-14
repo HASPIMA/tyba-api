83cddc2161440f01a6cc3d1ac82297ac83f56d2e
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 21:31:31 -0500

    Fix test case when query is not provided

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,25 @@
 import express, { Request } from 'express';
 
+import DataResponse from '../interfaces/DataResponse';
 import { getCurrentUserInfo } from '../middlewares/userInfo.middleware';
 
 const router = express.Router();
 
 // All routes refering to /**/users/* require getCurrentUserInfo middleware
 router.route('/*').all(getCurrentUserInfo);
+router.route('/nearby_search').get<Request, DataResponse>(async (req, res) => {
+  const response: DataResponse = {
+    data: null,
+    errors: [],
+  };
+
+  // We don't need user information at this point so we ignore it
+
+  //TODO: Call trip advisor's API
+
+  response.errors.push({ message: 'No query was provided' });
+
+  return res.status(400).json(response);
+});
 
 export default router;
