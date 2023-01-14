e8a46d54771a25c8f4e3754b7273b2f2fa68a93c
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 21:37:31 -0500

    Typesafe query parameters

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,25 +1,32 @@
 import express, { Request } from 'express';
 
 import DataResponse from '../interfaces/DataResponse';
+import NearbySearchQuery from '../interfaces/NearbySeachQuery';
 import { getCurrentUserInfo } from '../middlewares/userInfo.middleware';
 
 const router = express.Router();
 
 // All routes refering to /**/users/* require getCurrentUserInfo middleware
 router.route('/*').all(getCurrentUserInfo);
-router.route('/nearby_search').get<Request, DataResponse>(async (req, res) => {
+router.route('/nearby_search').get<Request, DataResponse, unknown, NearbySearchQuery>(async (req, res) => {
   const response: DataResponse = {
     data: null,
     errors: [],
   };
 
+  const query = req.query;
+
+  if (typeof query.latLong !== 'string') {
+    response.errors.push({ message: 'No query was provided or missing field' });
+
+    return res.status(400).json(response);
+  }
+
   // We don't need user information at this point so we ignore it
 
   //TODO: Call trip advisor's API
 
-  response.errors.push({ message: 'No query was provided' });
-
-  return res.status(400).json(response);
+  return res.status(500).json(response);
 });
 
 export default router;
