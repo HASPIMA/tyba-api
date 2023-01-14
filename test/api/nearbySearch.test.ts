e9da3d49b257a3a995b30bfd59c60bed405844b9
Harrison <harrison.pinto@d4d.com.co>
2023-01-13 22:33:03 -0500

    Create test case when valid query is given

--- test/api/nearbySearch.test.ts
+++ test/api/nearbySearch.test.ts
@@ -6,6 +6,8 @@ import { endpoints } from './constants';
 
 let token: string;
 
+const latLong = '42.3455,-71.10767';
+
 beforeAll(async () => {
   const userInfo = generateMockUser();
 
@@ -47,6 +49,29 @@ describe(`GET ${endpoints.nearbySearch}`, () => {
     expect(resBody.errors).toHaveLength(1);
 
     expect(nearbyRestaurants.statusCode).toBe(400);
+
+  });
+
+  it('should return restaurants if latituted and longitude is given', async () => {
+    // Get some restaurants
+    const nearbyRestaurants = await request(app)
+      .get(endpoints.nearbySearch)
+      .auth(token, { type: 'bearer' })
+      .set('Accept', 'application/json')
+      .query({ latLong })
+      .expect('Content-Type', /json/);
+
+    const resBody = nearbyRestaurants.body;
+
+    expect(resBody).toHaveProperty('data');
+    expect(resBody.data).not.toBeNull();
+    expect(resBody.data).toHaveProperty('length');
+    expect(resBody.data.length).toBeGreaterThan(0);
+
+    expect(resBody).toHaveProperty('errors');
+    expect(resBody.errors).toHaveLength(0);
+
+    expect(nearbyRestaurants.statusCode).toBe(200);
 
   });
 });
