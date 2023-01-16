18f79b907648dc80d889af3f4815901c81efb1eb
Harrison <harrison.pinto@d4d.com.co>
2023-01-16 04:49:41 -0500

    Add test case to allow users to be logged out
    This endpoint should work if the user is logged in

--- test/api/logout.test.ts
+++ test/api/logout.test.ts
@@ -5,6 +5,7 @@ import { generateMockUser, jwtRegex } from '../helpers';
 import { endpoints } from './constants';
 
 const userInfo = generateMockUser();
+let userToken: string;
 
 beforeAll(async () => {
   // Create new user in db
@@ -19,10 +20,39 @@ beforeAll(async () => {
 
 describe(`POST ${endpoints.logout}`, () => {
 
+  // Update user token for each test
+  beforeEach(async () => {
+    const login = await request(app)
+      .post(endpoints.login)
+      .set('Accept', 'application/json')
+      .send({ email: userInfo.email, password: userInfo.password })
+      .expect(200);
+
+    expect(login.body).toHaveProperty('data.token');
+    expect(login.body.data.token).toMatch(jwtRegex);
+
+    userToken = login.body.data.token;
+  });
+
   it('should have a connection to redis', async () => {
     const ping = await redisClient.PING();
 
     expect(ping).toBe('PONG');
+  });
+
+  it('should be sucessful when user is logged in', async () => {
+    const logout = await request(app)
+      .post(endpoints.logout)
+      .auth(userToken, { type: 'bearer' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/);
+
+    // Success status code
+    expect(logout.statusCode).toBe(200);
+
+    // No errors
+    expect(logout.body).toHaveProperty('errors');
+    expect(logout.body.errors).toHaveLength(0);
   });
 
 });
