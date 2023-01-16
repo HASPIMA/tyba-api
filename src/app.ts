282987ad3828e0331eafd7d9e52e87cf0ae541dc
Harrison <harrison.pinto@d4d.com.co>
2023-01-16 04:24:51 -0500

    Create client redis on server startup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { createClient } from 'redis';
 
 import * as middlewares from './middlewares/handlers.middleware';
 import api from './routes';
@@ -16,6 +17,24 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Setup redis client
+const client = createClient({ url: process.env.REDIS_CONNECTION_STRING });
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+async function connectToRedis() {
+  console.log('Connecting to Redis');
+  await client.connect();
+  console.log('Connected to Redis\n');
+}
+
+connectToRedis();
+
+// Provide redis client to each request
+app.use(async (_req, res, next) => {
+  res.locals.redisClient = client;
+  return next();
+});
+
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
@@ -27,4 +46,5 @@ app.use('/api/v1', api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
+export const redisClient = client;
 export default app;
